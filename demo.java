15ec73f6a79efc704379c5dd8fff336b3f3f23fe
abhishek <abhishekgoswami746@gmail.comgit config --global user.email abhishekgoswami746@gmail.com>
2023-03-24 15:43:08 +0530

    merger conflict resolve

--- demo.java
+++ demo.java
@@ -3,12 +3,12 @@ class Demo{
     int a=10;
     int b=20;
     int c=a+b;
-<<<<<<< HEAD
+
     
     int d=a/b;
-=======
-    int d=a%b;
->>>>>>> 91234a4b7604a49a85303fc40c28e5920d2a6f8f
+
+    int e=a%b;
+
     System.out.println("adding of number");
    
    }
